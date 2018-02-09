be3ff827add8d1b6928a057bd59685bbed4a0cbc
BWAbles <bables1@umbc.edu>
2018-02-09 15:39:26 -0500

    Third Commit

--- Assignment 1/Drawing 8/sketch.js
+++ Assignment 1/Drawing 8/sketch.js
@@ -1,10 +1,12 @@
 function setup() {
     createCanvas(2000, 2000);
+    background(255, 204, 0);
  
   }
   
   function draw() {
-
+    
+    fill(255, 255, 100);
     squares(100,500);
     squares(300,300);
       squares(500,500);
@@ -30,7 +32,6 @@ function setup() {
     squares(500,1300);
     squares(900,1300);
     squares(1300,1300);
-    
    
     
   }
@@ -39,14 +40,38 @@ function setup() {
       rect(x,y,200,200);
       ellipse(x+120,y+120,200,200);
       circles();
-      triangles();
+      triangles()
 
   function circles(){
+    fill(255, 204, 100);
     ellipse(100,100,100,100);
+    ellipse(200,100,100,100);
+    ellipse(100,200,100,100);
+    ellipse(100,300,100,100);
+    ellipse(100,400,100,100);
+    ellipse(300,100,100,100);
+    fill(255, 255, 100);
+    ellipse(400,100,100,100);
+    ellipse(100,1700,100,100);
+    ellipse(200,1700,100,100);
+    ellipse(300,1700,100,100);
+    ellipse(400,1700,100,100);
+    ellipse(500,1700,100,100);
+    ellipse(600,1700,100,100);
+    ellipse(700,1700,100,100);
+    ellipse(800,1700,100,100);
+    ellipse(900,1700,100,100);
+    ellipse(1000,1700,100,100);
+    ellipse(1100,1700,100,100);
+    ellipse(1200,1700,100,100);
+    ellipse(1300,1700,100,100);
+    ellipse(1400,1700,100,100);
+    ellipse(1500,1700,100,100);
+    ellipse(1600,1700,100,100);
   }
 
 
   function triangles(){
-    squares(x+100,y+100, 150,150,300,300)
+    ellipse(100,100,100,100);
   }
   }
